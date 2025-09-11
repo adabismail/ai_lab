69fd88aab35d2d6cdd12f2b842e21ec76fb35d50
adabismail <adabismail000@gmail.com>
2025-09-11 21:33:16 +0530

    Initial commit for ai_lab

---
+++ lab0.py
@@ -0,0 +1,24 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+size = int(input("Enter number of elements: "))
+arr = list(map(int, input("Enter elements: ").split()))
+
+print("Original array:", arr)
+print("Bubble Sort:", bubble_sort(arr.copy()))
+print("Selection Sort:", selection_sort(arr.copy()))
